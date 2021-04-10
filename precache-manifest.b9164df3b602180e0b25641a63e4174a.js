a6d0b1d6aa2d0315ba577b518999637b15d6ef4e
ypetrenko <yevhenii.petrenko@altran.com>
2021-04-10 16:58:47 +0200

    inital

---
+++ precache-manifest.b9164df3b602180e0b25641a63e4174a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f0e98b6ab963312122c6eeb6243b3ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40b317ee026113f9dc62",
+    "url": "/static/css/main.0fbd6a99.chunk.css"
+  },
+  {
+    "revision": "d80e5042651d718a9154",
+    "url": "/static/js/2.ba001b0a.chunk.js"
+  },
+  {
+    "revision": "e8a4be6b3f10de7030aea16af327bb06",
+    "url": "/static/js/2.ba001b0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40b317ee026113f9dc62",
+    "url": "/static/js/main.a0b23707.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
